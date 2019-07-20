2993db7128f0097af37acfe8382976f93a72bbb1
Jamena McInteer <jamena@webdevabq.com>
2019-07-20 15:03:26 -0500

    updated site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `jamena.dev`,
+    description: `Hello, I'm Jamena McInteer. I’m a full stack developer and UI/UX designer from Austin, TX. I like to build beautiful things that make a difference.`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#DEBDC2`,
+        theme_color: `#DEBDC2`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
