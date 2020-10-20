6654006728afb4d4e8a7bdf26f940092b659c4ba
Jamena McInteer <ndjamenamarmon@gmail.com>
2020-10-20 16:56:54 -0500

    Update talks page (#15)

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -21,25 +21,7 @@ const TalksPage = () => {
 
       <PageSection>
         <PageSectionHeader>Upcoming Talks</PageSectionHeader>
-        <table>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Event</th>
-              <th>Date</th>
-              <th>City</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td><a href="https://github.com/jamenamcinteer/the-state-of-css-in-vue">The State of CSS in Vue</a></td>
-              <td><a href="https://vueconf.us/">VueConf US</a></td>
-              <td>March 3, 2020</td>
-              <td>Austin, TX</td>
-            </tr>
-          </tbody>
-        </table>
-        {/* <p>No talks currently scheduled.</p> */}
+        <p>No talks currently scheduled.</p>
       </PageSection>
 
       <PageSection>
@@ -55,13 +37,37 @@ const TalksPage = () => {
           </thead>
           <tbody>
             <tr>
-              <td><a href="https://github.com/jamenamcinteer/Workshop-Intro-to-Responsive-Design-with-CSS-Grid-and-Flexbox">Intro to Responsive Design with CSS Grid and Flexbox</a></td>
-              <td><a href="https://www.eventbrite.com/e/5th-austin-diversity-hackathon-atxdivhack-tickets-62292670053#">Austin Diversity Hackathon</a></td>
+              <td>
+                <a href="https://www.vuemastery.com/conferences/vueconf-us-2020/the-state-of-css-in-vue/">
+                  The State of CSS in Vue
+                </a>
+              </td>
+              <td>
+                <a href="https://vueconf.us/">VueConf US</a>
+              </td>
+              <td>March 3, 2020</td>
+              <td>Austin, TX</td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://github.com/jamenamcinteer/Workshop-Intro-to-Responsive-Design-with-CSS-Grid-and-Flexbox">
+                  Intro to Responsive Design with CSS Grid and Flexbox
+                </a>
+              </td>
+              <td>
+                <a href="https://www.eventbrite.com/e/5th-austin-diversity-hackathon-atxdivhack-tickets-62292670053#">
+                  Austin Diversity Hackathon
+                </a>
+              </td>
               <td>September 21, 2019</td>
               <td>Austin, TX</td>
             </tr>
             <tr>
-              <td><a href="https://docs.google.com/presentation/d/1WycAwgJRewixuzRgcHTkEmmvfi9p0Cxu6FqCSQ9UsoY/edit?usp=sharing">Front End Web Development</a></td>
+              <td>
+                <a href="https://docs.google.com/presentation/d/1WycAwgJRewixuzRgcHTkEmmvfi9p0Cxu6FqCSQ9UsoY/edit?usp=sharing">
+                  Front End Web Development
+                </a>
+              </td>
               <td>TEKsystems Lunch & Learn</td>
               <td>September 2016</td>
               <td>Albuquerque, NM</td>
@@ -71,7 +77,10 @@ const TalksPage = () => {
       </PageSection>
 
       <PageSection>
-        <p>If you are interested in booking me to speak at your conference or meetup, please <Link to="/contact">get in touch</Link>. Thank you!</p>
+        <p>
+          If you are interested in booking me to speak at your conference or
+          meetup, please <Link to="/contact">get in touch</Link>. Thank you!
+        </p>
       </PageSection>
     </>
   )
